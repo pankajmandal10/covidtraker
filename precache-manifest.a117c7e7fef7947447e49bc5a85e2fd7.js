b906defae1830d428e3ab34f91916e2eb8a511d1
pankajmandal1996 <mandalpankaj10696@gmail.com>
2021-02-08 12:28:51 +0530

    Updates

---
+++ precache-manifest.a117c7e7fef7947447e49bc5a85e2fd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c682bc33a34034908e4d8019e492bc0",
+    "url": "/covidtraker/index.html"
+  },
+  {
+    "revision": "ab7d1d9fbd7d75f9dada",
+    "url": "/covidtraker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "089b9a0ed88ddbff1eab",
+    "url": "/covidtraker/static/css/main.ea3e3ad4.chunk.css"
+  },
+  {
+    "revision": "ab7d1d9fbd7d75f9dada",
+    "url": "/covidtraker/static/js/2.d03b490a.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covidtraker/static/js/2.d03b490a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "089b9a0ed88ddbff1eab",
+    "url": "/covidtraker/static/js/main.98c0a2c4.chunk.js"
+  },
+  {
+    "revision": "1ef8a618fbac4ae75c37",
+    "url": "/covidtraker/static/js/runtime-main.a8d54c4d.js"
+  }
+]);
